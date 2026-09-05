b77f429aba0d7d882041d397ad3214bd9dc27c1c
forbidenperson <321699103+wahyyrmdann@users.noreply.github.com>
2026-09-05 23:58:31 +0800

    fix: hapus folder kampuslms-B-kelompok-07 dan berkas eksperimen non-docs

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CourseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-/*
-|--------------------------------------------------------------------------
-| Course Routes
-|--------------------------------------------------------------------------
-| Route untuk modul mata kuliah.
-| Menggunakan nama route agar URL tidak hardcode dan mudah diubah.
-|--------------------------------------------------------------------------
-*/
-
-// Menampilkan daftar mata kuliah
-Route::post('/mata-kuliah', [CourseController::class, 'index'])
-    ->name('mata-kuliah.index');
-
-// Menampilkan detail satu mata kuliah
-Route::post('/mata-kuliah/{id}', [CourseController::class, 'show'])
-    ->name('mata-kuliah.show');
-
-Route::get('/tentang', function () {
-    return view('tentang');
-})->name('tentang');
